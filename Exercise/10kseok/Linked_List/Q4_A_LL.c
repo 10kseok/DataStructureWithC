0644097b4ce16a83172145650cb1336efbdfc0cf
koesnam <76582376+10kseok@users.noreply.github.com>
2024-02-13 15:19:05 +0900

    연결리스트 Q4 해결
    홀수를 뒤로 보내는 함수와 동일한 로직을 사용하였습니다.

---
+++ Exercise/10kseok/Linked_List/Q4_A_LL.c
@@ -0,0 +1,254 @@
+//////////////////////////////////////////////////////////////////////////////////
+
+/* CE1007/CZ1007 Data Structures
+Lab Test: Section A - Linked List Questions
+Purpose: Implementing the required functions for Question 4 */
+
+//////////////////////////////////////////////////////////////////////////////////
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//////////////////////////////////////////////////////////////////////////////////
+
+typedef struct _listnode
+{
+	int item;
+	struct _listnode *next;
+} ListNode;			// You should not change the definition of ListNode
+
+typedef struct _linkedlist
+{
+	int size;
+	ListNode *head;
+} LinkedList;			// You should not change the definition of LinkedList
+
+
+//////////////////////// function prototypes /////////////////////////////////////
+
+// You should not change the prototype of this function
+void moveEvenItemsToBack(LinkedList *ll);
+
+void printList(LinkedList *ll);
+void removeAllItems(LinkedList *ll);
+ListNode * findNode(LinkedList *ll, int index);
+int insertNode(LinkedList *ll, int index, int value);
+int removeNode(LinkedList *ll, int index);
+
+//////////////////////////// main() //////////////////////////////////////////////
+
+int main()
+{
+	LinkedList ll;
+	int c, i, j;
+	c = 1;
+	//Initialize the linked list 1 as an empty linked list
+	ll.head = NULL;
+	ll.size = 0;
+
+
+	printf("1: Insert an integer to the linked list:\n");
+	printf("2: Move all even integers to the back of the linked list:\n");
+	printf("0: Quit:\n");
+
+	while (c != 0)
+	{
+		printf("Please input your choice(1/2/0): ");
+		scanf("%d", &c);
+
+		switch (c)
+		{
+		case 1:
+			printf("Input an integer that you want to add to the linked list: ");
+			scanf("%d", &i);
+			j = insertNode(&ll, ll.size, i);
+			printf("The resulting linked list is: ");
+			printList(&ll);
+			break;
+		case 2:
+			moveEvenItemsToBack(&ll); // You need to code this function
+			printf("The resulting linked list after moving even integers to the back of the linked list is: ");
+			printList(&ll);
+			removeAllItems(&ll);
+			break;
+		case 0:
+			removeAllItems(&ll);
+			break;
+		default:
+			printf("Choice unknown;\n");
+			break;
+		}
+	}
+	return 0;
+}
+
+//////////////////////////////////////////////////////////////////////////////////
+
+void moveEvenItemsToBack(LinkedList *ll)
+{
+	// 짝수는 맨 뒤로 보내는 함수
+	ListNode *cur_node = ll->head;
+	ListNode *prev_node = NULL;
+	int count = ll->size;
+	while (count > 0) {
+		if (cur_node->next == NULL) {
+			break;
+		}
+		int item = cur_node->item;
+		// 짝수인 경우, 이전값만 기록하고 다음 노드로 이동
+		if (item % 2 == 1) {
+			prev_node = cur_node;
+			cur_node = cur_node->next;
+			count--;
+			continue;
+		}
+		// 홀수인 경우
+		// - 현재 노드가 헤드인지 아닌지에 따라 다르게 처리
+		ListNode *even_node, *next_node;
+		even_node = cur_node;
+		if (prev_node == NULL) {
+			// 현재 노드가 헤드거나, 홀수만 있는 경우
+			// - 노드가 하나면 즉시 종료.
+			ll->head = cur_node->next;
+			next_node = ll->head;
+		} else {
+			prev_node->next = even_node->next;
+			next_node = even_node->next;
+		}
+
+		// - (공통) 짝수를 마지막 노드로 연결해준다.
+		while (cur_node->next != NULL) {
+			cur_node = cur_node->next;
+		}
+		cur_node->next = even_node;
+		cur_node->next->next = NULL;
+		count--;
+		// 다음으로 순회할 노드로 변경
+		cur_node = next_node;
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////////////
+
+void printList(LinkedList *ll){
+
+	ListNode *cur;
+	if (ll == NULL)
+		return;
+	cur = ll->head;
+
+	if (cur == NULL)
+		printf("Empty");
+	while (cur != NULL)
+	{
+		printf("%d ", cur->item);
+		cur = cur->next;
+	}
+	printf("\n");
+}
+
+
+void removeAllItems(LinkedList *ll)
+{
+	ListNode *cur = ll->head;
+	ListNode *tmp;
+
+	while (cur != NULL){
+		tmp = cur->next;
+		free(cur);
+		cur = tmp;
+	}
+	ll->head = NULL;
+	ll->size = 0;
+}
+
+
+ListNode *findNode(LinkedList *ll, int index){
+
+	ListNode *temp;
+
+	if (ll == NULL || index < 0 || index >= ll->size)
+		return NULL;
+
+	temp = ll->head;
+
+	if (temp == NULL || index < 0)
+		return NULL;
+
+	while (index > 0){
+		temp = temp->next;
+		if (temp == NULL)
+			return NULL;
+		index--;
+	}
+
+	return temp;
+}
+
+int insertNode(LinkedList *ll, int index, int value){
+
+	ListNode *pre, *cur;
+
+	if (ll == NULL || index < 0 || index > ll->size + 1)
+		return -1;
+
+	// If empty list or inserting first node, need to update head pointer
+	if (ll->head == NULL || index == 0){
+		cur = ll->head;
+		ll->head = malloc(sizeof(ListNode));
+		ll->head->item = value;
+		ll->head->next = cur;
+		ll->size++;
+		return 0;
+	}
+
+
+	// Find the nodes before and at the target position
+	// Create a new node and reconnect the links
+	if ((pre = findNode(ll, index - 1)) != NULL){
+		cur = pre->next;
+		pre->next = malloc(sizeof(ListNode));
+		pre->next->item = value;
+		pre->next->next = cur;
+		ll->size++;
+		return 0;
+	}
+
+	return -1;
+}
+
+
+int removeNode(LinkedList *ll, int index){
+
+	ListNode *pre, *cur;
+
+	// Highest index we can remove is size-1
+	if (ll == NULL || index < 0 || index >= ll->size)
+		return -1;
+
+	// If removing first node, need to update head pointer
+	if (index == 0){
+		cur = ll->head->next;
+		free(ll->head);
+		ll->head = cur;
+		ll->size--;
+
+		return 0;
+	}
+
+	// Find the nodes before and after the target position
+	// Free the target node and reconnect the links
+	if ((pre = findNode(ll, index - 1)) != NULL){
+
+		if (pre->next == NULL)
+			return -1;
+
+		cur = pre->next;
+		pre->next = cur->next;
+		free(cur);
+		ll->size--;
+		return 0;
+	}
+
+	return -1;
+}
